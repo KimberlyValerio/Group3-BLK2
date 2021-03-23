79c2742eae9a85048b61284969bda3628b24ec86
erikajoycemosada <80941655+erikajoycemosada@users.noreply.github.com>
2021-03-23 14:29:14 +0800

    Add files via upload

--- Arithmetic Operation/subtraction.php
+++ Arithmetic Operation/subtraction.php
@@ -1,196 +1,257 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <style type="text/css">
-    h1,h2{
-      text-align: center;x
-    }
-    .questions{
-      padding: 10px;
-      border-style: solid;
-      border-width: 1px;
-      color: black;
-      border-radius: 5px;
-      margin-bottom: 20px;
-    }
-    .box{
-    padding: 50px;
-    border: 1px solid gray;
-    border-radius: 20px;
-    background: rgba(255,255,255,.2);
-    max-width: 600px;
-    margin: auto;
-    margin-top: 50px;
-    font-size: 18px;
-    font-weight: bold;
-    justify-content: center;
-    color: black;
-  }
-  ul{
-      list-style-type: none;
-    }
-  </style>
+  	h1,h2{
+  		text-align: center;x
+  	}
+  	.questions{
+  		padding: 10px;
+  		border-style: solid;
+  		border-width: 1px;
+  		color: black;
+  		border-radius: 5px;
+  	}
+  	.box{
+		padding: 50px;
+		border: 1px solid gray;
+		border-radius: 20px;
+		background: rgba(255,255,255,.2);
+		max-width: 600px;
+		margin: auto;
+		margin-top: 50px;
+		font-size: 18px;
+		font-weight: bold;
+		justify-content: center;
+		color: black;
+	}
+	</style>
 </head>
 <body>
 
 <div class="container-fluid">
   <div class="row">
-    <h1>KIDS MATH-TYNICS</h1>
-
-    <!-- NAVIGATION -->
-
+  	<h1>KIDS MATH-TYNICS</h1>
     <div class="col-sm-3" id="nav" >
-      <h2>CATEGORY</h2>
-     <ul>
-          <li><a href="Page1.php">HOME</a></li>
-          <li><a href="addition.php">ADDITION</a></li>
-          <li><a href="subtraction.php">SUBTRACTION</a></li>
-          <li><a href="mul.php">MULTIPLICATION</a></li>
-          <li><a href="div.php">DIVISION</a></li>
-        </ul>
+    	<h2>CATEGORY</h2>
+			<ul>
+  			<li><a href="Page1.php">HOME</a></li>
+  			<li><a href="">ADDITION</a></li>
+  			<li><a href="subtraction.php">SUBTRACTION</a></li>
+  			<li><a href="multiplication.php">MULTIPLICATION</a></li>
+  			<li><a href="">DIVISION</a></li>
+			</ul>
     </div>
 
-    <!-- QUESTIONAIRES -->
     <div class="col-sm-6">
-      <h3>Let's learn SUBTRACTION</h3>
-      <p> Subtraction, subtract means to take away from a group or a number of things. When we subtract, the number of things in the group reduce or become less.
-        </br>Example:</br>&nbsp;&nbsp;&nbsp;&nbsp;Mark has a book which contains 200 pages. He has already read 99 pages. How many pages are unread?<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SOLUTION: 200 - 99 = 101 unread pages<br></p>
+    	<h3>Let's learn SUBTRACTION</h3>
+    	<p> In math, to subtract means to take away from a group or a number of things. When we subtract, the number of things in the group reduce or become less. The minuend, subtrahend and difference are parts of a subtraction problem.
+        </br>Example:</br>&nbsp;&nbsp;&nbsp;&nbsp;There were 8 beach balls but 5 of them were damaged. How many beach balls were left?<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SOLUTION: 8 beach balls - 5 damaged balls = 3 balls <br></p>
 
         <h3>NOW ITS YOUR TURN</h3>
 
         <div class="questions">
-          <p> 7 candles were burning on a cake. Jake blew 3 of them out. How many were left burning?</p>
-            <form action="" method="post">
-              <input type="text" name="num1">
-              <input type="submit" name="check1" value="Check">  
-        </form>
-          <?php
-          if(isset($_POST['check1'])){
+        	<p>Seven candles were burning on a cake. Jake blew three of them out. How many were left burning?</p>
+        		<form action="" method="post">
+          		<input type="text" name="num">
+           		<button onclick="num1()">Check</button>
 
-           $num1 =$_POST['num1'];
-           if ($num1 == "4") {
-            echo "Your answer is correct";
-           }
-           elseif ($num1 !="4") {
-            echo "Try Again";
-           }
-          }
-          ?>
+              <script>
+              function num1() {
+              alert("The answer is: 4 candles left");
+              }
+              </script>  
+        </form>
         </div>
 
         <div class="questions">
-          <p>A shopkeeper bought 200 eggs and sold 100 eggs. How many eggs were left unsold?</p>
-            <form action="" method="post">
-              <input type="text" name="num2">
-              <input type="submit" name="check2" value="Check">  
-        </form>
-          <?php
-          if(isset($_POST['check2'])){
+        	<p>Seven candles were burning on a cake. Jake blew some of them out to leave four burning. How many did he blow out?</p>
+        		<form action="" method="post">
+          		<input type="text" name="num1">
+           		<button onclick="num2()">Check</button>
 
-           $num2 =$_POST['num2'];
-           if ($num2 == "100") {
-            echo "Your answer is correct";
-           }
-           elseif ($num2 !="100") {
-            echo "Try Again";
-           }
-          }
-          ?>
-        </div>
-        <div class="questions">
-          <p>Dora got $325 from her father and $289 from her mother. How much money does she have now?</p>
-            <form action="" method="post">
-              <input type="text" name="num3">
-              <input type="submit" name="check3" value="Check">  
+              <script>
+              function num2() {
+              alert("The answer is:  3 candles");
+              }
+              </script> 
         </form>
-          <?php
-          if(isset($_POST['check3'])){
-
-           $num3 =$_POST['num3'];
-           if ($num3 == "$614"||"614") {
-            echo "Your answer is correct";
-           }
-           elseif ($num3 !="614") {
-            echo "Try Again";
-           }
-          }
-          ?>
         </div>
 
         <div class="questions">
-          <p>An animal care society tested 20 pet animals. 7 were infected by diseases. Find the number of healthy pet animals that participated in the medical tests?</p>
-            <form action="" method="post">
-              <input type="text" name="num4">
-              <input type="submit" name="check4" value="Check">  
-        </form>
-          <?php
-          if(isset($_POST['check4'])){
+        	<p>A shopkeeper bought 240 eggs and sold 148 eggs. How many eggs were left unsold?</p>
+        		<form action="" method="post">
+          		<input type="text" name="num1">
+           		<button onclick="num3()">Check</button>
 
-           $num4 =$_POST['num4'];
-           if ($num4 == "13") {
-            echo "Your answer is correct";
-           }
-           elseif ($num4 !="13") {
-            echo "Try Again";
-           }
-          }
-          ?>
+              <script>
+              function num3() {
+              alert("The answer is:  92 eggs left");
+              }
+              </script>  
+        </form>
         </div>
 
         <div class="questions">
-          <p>In an annual celebration, 10 students participated. Of them, 6 were boys. Find the number of girls who participated.</p>
-            <form action="" method="post">
-              <input type="text" name="num5">
-              <input type="submit" name="check5" value="Check">  
-        </form>
-          <?php
-          if(isset($_POST['check5'])){
+        	<p>An animal care society tested 356 pet animals. 127 were infected by diseases. Find the number of healthy pet animals that participated in the medical tests?
+</p>
+        		<form action="" method="post">
+          		<input type="text" name="num1">
+           		<button onclick="num4()">Check</button>
 
-           $num5 =$_POST['num5'];
-           if ($num5 == "4") {
-            echo "Your answer is correct";
-           }
-           elseif ($num5 !="4") {
-            echo "Try Again";
-           }
-          }
-          ?>
+              <script>
+              function num4() {
+              alert("The answer is:  226 healhty animals");
+              }
+              </script> 
+        </form>
+        </div>
+
+        <div class="questions">
+        	<p>In an annual celebration, 674 students participated. Of them, 392 were boys. Find the number of girls who participated.</p>
+        		<form action="" method="post">
+          		<input type="text" name="num1">
+           		<button onclick="num5()">Check</button>
+
+              <script>
+              function num5() {
+              alert("The answer is:  282 girls");
+              }
+              </script>
+        </form>
+        </div>
+
+        <div class="questions">
+        	<p>Mark has a book which contains 649 pages. He has already read 495 pages. How many pages are unread?</p>
+        		<form action="" method="post">
+          		<input type="text" name="num1">
+           		<button onclick="num6()">Check</button>
+
+              <script>
+              function num6() {
+              alert("The answer is:  154 pages");
+              }
+              </script>
+        </form>
+        </div>
+
+        <div class="questions">
+        	<p>Director James directed a film which ran for 248 minutes. During the editing process, 109 minutes were removed. What is the final running time of the film, after editing?</p>
+        		<form action="" method="post">
+          		<input type="text" name="num1">
+           		<button onclick="num7()">Check</button>
+
+              <script>
+              function num7() {
+              alert("The answer is:  139 minutes");
+              }
+              </script>  
+        </form>
+        </div>
+
+        <div class="questions">
+        	<p>There are 5718 DVDs in Mr. Miller’s shop. 2199 are audio DVDs and the rest of them are video DVDs. Find the number of video DVDs in Mr. Miller’s shop.</p>
+        		<form action="" method="post">
+          		<input type="text" name="num1">
+           		<button onclick="num8()">Check</button>
+
+              <script>
+              function num8() {
+              alert("The answer is:  3519 video DVDs");
+              }
+              </script>  
+        </form>
+        </div>
+
+        <div class="questions">
+        	<p>Clara withdrew $ 6789 from her account. The initial amount in her account was $ 8790. Find the balance left after the withdrawal.</p>
+        		<form action="" method="post">
+          		<input type="text" name="num1">
+              <button onclick="num9()">Check</button>
+
+              <script>
+              function num9() {
+              alert("The answer is:  $2001");
+              }
+              </script>
+        </form>
+        </div>
+
+        <div class="questions">
+        	<p>A free medical camp was conducted in Mexico. 1278 males participated in the camp. The entry book shows 4012 people participated in the camp. Find the number of females who participated.</p>
+        		<form action="" method="post">
+          		<input type="text" name="num1">
+              <button onclick="num10()">Check</button>
+
+              <script>
+              function num10() {
+              alert("The answer is: 2734 females");
+              }
+              </script>
+            </form>        
+        </div>
+
+        <div class="questions">
+        	<p>Cathy needs at least 2000 points to go to level 2 in a video game. She has only 1254 points in level 1. How many more points does she need to qualify for level 2?</p>
+        		<form action="" method="post">
+          		<input type="text" name="num1">
+              <button onclick="num11()">Check</button>
+
+              <script>
+              function num11() {
+              alert("The answer is: 746 points");
+              }
+              </script>
+        </form>
+        </div>
+
+        <div class="questions">
+        	<p>The average distance from earth to the sun is 92, 589, 230 miles. The distance from earth to the moon is 92,350,373 miles less than the distance from earth to the sun. Find the distance from earth to the moon.</p>
+        		<form action="subtraction.php" method="post">
+          		<input type="text" name="num1">
+              <button onclick="num12()">Check</button>
+
+              <script>
+              function num12() {
+              alert("The answer is: 9275019372 miles");
+              }
+              </script>
+            </form>
         </div>
 
     </div>
 
-    <!-- CALCULATOR -->
     <div class="col-sm-3" id="calculator">
-      <h2>Find the Difference. !</h2>
-      <div class="box">
-      <h1 id="answer"></h1>
-      Input a number<input type="text" id="num1">
-      <br> <br>
-      Input a number<input type="text" id="num2">
-      <br>
-      <br>
+    	<h2>Find the Difference !</h2>
+    	<div class="box">
+			<h1 id="answer"></h1>
+			Input a number<input type="text" id="num1">
+			<br> <br>
+			Input a number<input type="text" id="num2">
+			<br>
+			<br>
 
-      <button onclick="calculate()">Calculate</button>
+			<button onclick="calculate()"> Calculate</button>
 
-      <script>
-          
-      function calculate() {
-        var field1=document.getElementById("num1").value;
-        var field2=document.getElementById("num2").value;
+			<script>
+					
+			function calculate() {
+				var field1=document.getElementById("num1").value;
+				var field2=document.getElementById("num2").value;
 
-        var result=parseFloat(field1)-parseFloat(field2);
+				var result=parseFloat(field1)-parseFloat(field2);
 
-        if(!isNaN(result)){
-            document.getElementById("answer").innerHTML="The difference. of " + field1 + " and " + field2 + " is " + result;
-        }
-        }
-      </script>
-    </div>
-              
-          
+				if(!isNaN(result)){
+						document.getElementById("answer").innerHTML="The difference of " + field1 + " and " + field2 + " is " + result;
+				}
+				}
+			</script>
+		</div>
+				    	
+				  
 </body>
 </html>
